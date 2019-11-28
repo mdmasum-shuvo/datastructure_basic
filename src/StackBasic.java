8c28707a2491fbc0280714b4ea467507fc5c0490
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2019-11-28 17:28:43 +0600

    Initial commit

---
+++ src/StackBasic.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class StackBasic {
+
+    public static int[] myStack = new int[10];
+    public static int top = -1;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < myStack.length; i++) {
+            int input = scanner.nextInt();
+            push(input);
+        }
+        printStack();
+        while (true) {
+            String pop = scanner.next();
+            if (top < 0)
+                break;
+            if (pop.equalsIgnoreCase("pop")) {
+                pop();
+                printStack();
+            }
+        }
+    }
+
+    private static void pop() {
+        top--;
+    }
+
+    private static void printStack() {
+        System.out.print("output: ");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(myStack[i] + "\t");
+        }
+    }
+
+    private static void push(int number) {
+        if (top >= myStack.length - 1) {
+            return;
+        }
+        myStack[++top] = number;
+    }
+}
