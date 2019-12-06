1268ef10346fe42b138231ce108de56dc5ba3194
mdmasum.shuvo.cse@gmail.com <mdmasum.shuvo.cse@gmail.com>
2019-12-06 17:27:00 +0600

    LinkListBasic

--- src/LinkList/LinkListBasic.java
+++ src/LinkList/LinkListBasic.java
@@ -14,12 +14,29 @@ public class LinkListBasic {
 
     }
 
-    public void pintList() {
-        Node node = head;
+    public void pintList(Node newNode) {
+        Node node = newNode;
         while (node != null) {
             System.out.print(node.data + "\t");
             node = node.next;
         }
+    }
+
+    public Node addNodeToHead(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        return newNode;
+    }
+
+    public Node addNoteToLast(int data) {
+        Node newNode = new Node(data);
+        return newNode;
+    }
+
+    public void addNodeAfterGivenNode(Node givenNode,int data){
+        Node node=new Node(data);
+        node.next=givenNode.next;
+        givenNode.next=node;
     }
 
     public static void main(String[] args) {
@@ -28,12 +45,12 @@ public class LinkListBasic {
         linkList.head = new Node(1);
         Node second = new Node(2);
         Node third = new Node(3);
-
+        third.next = linkList.addNoteToLast(10);
         //link among the object
-
         linkList.head.next = second;
         second.next = third;
-        linkList.pintList();
+        linkList.addNodeAfterGivenNode(second,100);
+        linkList.pintList(linkList.head);
 
     }
 }
