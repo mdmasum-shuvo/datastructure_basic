9b7664d7493107ab60083085a8889f6868381fd1
mdmasum.shuvo.cse@gmail.com <mdmasum.shuvo.cse@gmail.com>
2019-12-08 02:39:56 +0600

    LinkListBasic insert dynamic data

---
+++ src/LinkList/Solution.java
@@ -0,0 +1,90 @@
+package LinkList;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+    private static SinglyLinkedListNode last;
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) throws IOException {
+        while (node != null) {
+            System.out.println(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.println(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+        if (head == null) {
+            head = node;
+            last = head;
+        } else {
+            last.next=node;
+        }
+        last=node;
+
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+
+            llist.head = llist_head;
+        }
+
+
+        printSinglyLinkedList(llist.head, "\n");
+        scanner.close();
+    }
+}
